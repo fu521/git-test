568d55d21af7c19af21001465301ea0d3c2cc95a
fuguang <fuguang@163.com>
2021-09-27 10:39:50 +0800

    first  commit

---
+++ src/main/java/com/fuguang/mapper/score.java
@@ -0,0 +1,7 @@
+package com.fuguang.mapper;
+
+public class score {
+    public static void main(String[] args) {
+        System.out.println("fuguang");
+    }
+}
---
+++ src/main/java/com/fuguang/pojo/student.java
@@ -0,0 +1,8 @@
+package com.fuguang.pojo;
+
+public class student {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("word");
+    }
+}
