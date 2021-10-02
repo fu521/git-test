55bd1e2bf439aecf91956cbd8787c032387f3406
fuguang <fuguang@163.com>
2021-10-02 10:05:32 +0800

    hot-fix1

--- src/main/java/com/fuguang/mapper/score.java
+++ src/main/java/com/fuguang/mapper/score.java
@@ -5,5 +5,6 @@ public class score {
         System.out.println("fuguang");
         System.out.println("hello git");
         System.out.println("hello git3");
+        System.out.println("hot-fix1");
     }
 }
