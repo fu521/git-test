a5fb8c7ba12b41a59f621fc755e61591adabecf8
fuguang <fuguang@163.com>
2021-10-02 09:16:05 +0800

    second commit

--- src/main/java/com/fuguang/mapper/score.java
+++ src/main/java/com/fuguang/mapper/score.java
@@ -3,5 +3,6 @@ package com.fuguang.mapper;
 public class score {
     public static void main(String[] args) {
         System.out.println("fuguang");
+        System.out.println("hello git");
     }
 }
